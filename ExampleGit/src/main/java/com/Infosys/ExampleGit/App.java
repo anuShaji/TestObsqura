183ff632a4b1a8b8cccc526e221df6866a6ac2f0
ANUPAMA VISHNU <ANUPAMA VISHNU@LAPTOP-QEKB9P62>
2024-05-09 22:02:53 +0400

    second commit

---
+++ ExampleGit/src/main/java/com/Infosys/ExampleGit/App.java
@@ -0,0 +1,13 @@
+package com.Infosys.ExampleGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
